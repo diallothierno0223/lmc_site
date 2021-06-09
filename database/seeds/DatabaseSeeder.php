a4004360fd3e7faf9a02c87ca63bb5707bdabc52
thierno <thiernosaliou0223@gmail.com>
2021-06-09 11:19:02 +0000

    partage db

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,12 +18,12 @@ class DatabaseSeeder extends Seeder
             'title' => "title seeder",
             'subtitle' => "subtitle seeder",
             'article' => "  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa excepturi iste beatae maxime quasi sint velit, earum eius animi debitis rem. Magnam assumenda in ab earum, exercitationem quas ipsa asper   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa excepturi iste beatae maxime quasi sint velit, earum eius animi debitis rem. Magnam assumenda in ab earum, exercitationem quas ipsa aspernatur.natur.",
-            'image' => "journals\June2021\gqkRawlwxfgaM5MDbY8D.png",
+            'image' => "journals\June2021\IejnrH6fbqrNuKfY8doH.jpg",
         ];
         $data2 = [
             'title' => "test title",
             'description' => "description test",
-            'image' => "journals\June2021\gqkRawlwxfgaM5MDbY8D.png",
+            'image' => "journals\June2021\IejnrH6fbqrNuKfY8doH.jpg",
         ];
         // $this->call(UserSeeder::class);
         for($i = 0; $i < 31; $i++){
